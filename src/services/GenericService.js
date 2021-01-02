e934df1d40226d6de4912da36e3eb73660c565c4
Shaheer2299 <72572695+Shaheer2299@users.noreply.github.com>
2021-01-02 15:13:41 +0500

    1

--- src/services/GenericService.js
+++ src/services/GenericService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:4000/";
+axios.defaults.baseURL = "https://drexel-api.herokuapp.com/";
 axios.defaults.headers.common["x-auth-token"] = localStorage.getItem("token");
 class GenericService {
 
